b30f0558647f327c1110ab24f7206a85b5a351ac
izaiascrs <izaiascrs@gmail.com>
2025-09-29 19:39:23 -0300

    add cors origins

--- src/index.js
+++ src/index.js
@@ -8,11 +8,29 @@ require('dotenv').config();
 
 const app = express();
 
+const development = process.env.NODE_ENV !== 'production';
+const allowedOrigins = process.env.ALLOW_ORIGINS ? process.env.ALLOW_ORIGINS.split(',') : [];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // in development allow all origins
+        if (development) {
+            callback(null, true)
+        } // in production only allow requests from allowed origins
+        else if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS', { cause: "cors"}), false);
+        }
+    },
+    optionsSuccessStatus: 200,
+}
+
 app.use(helmet());
 app.set('trust proxy', 1);
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
@@ -20,6 +38,14 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/', api);
+
+app.use((err, req, res, next) => {
+    console.error(err?.message);
+    if (err.cause === "cors") {
+        return res.status(403).json({ message: err.message });
+    }
+    res.status(500).json({ message: 'Internal server error' });
+})
 
 const PORT = process.env.PORT || 3000;
 
